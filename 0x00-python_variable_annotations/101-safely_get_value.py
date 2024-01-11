c3dc0e492b10c45880f2e512500705fe4af98733
Ab-Ruh <abell.demere@gmail.com>
2024-01-12 00:36:14 +0300

    pyback

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,10 +3,13 @@
 this module contains the sum_mixed_list function
 '''
 from typing import Mapping, Any, TypeVar, Union
+
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
     '''
     function
     '''
